4689627cda33346603c06c918d7d65d9df3390f6
James Healy <james@yob.id.au>
2019-08-14 21:32:25 +1000

    initial commit
    * use go modules to manage dependencies
    * a docker workflow for consistent building
    
    To run a test server:
    
        ./auto/run

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/secmask/go-redisproto"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	parser := redisproto.NewParser(conn)
+	writer := redisproto.NewWriter(bufio.NewWriter(conn))
+	var ew error
+	for {
+		command, err := parser.ReadCommand()
+		if err != nil {
+			_, ok := err.(*redisproto.ProtocolError)
+			if ok {
+				ew = writer.WriteError(err.Error())
+			} else {
+				log.Println(err, " closed connection to ", conn.RemoteAddr())
+				break
+			}
+		} else {
+			cmd := strings.ToUpper(string(command.Get(0)))
+			switch cmd {
+			case "GET":
+				ew = writer.WriteBulkString("dummy")
+			case "SET":
+				ew = writer.WriteBulkString("OK")
+			default:
+				ew = writer.WriteError("Command not support")
+			}
+		}
+		if command.IsLast() {
+			writer.Flush()
+		}
+		if ew != nil {
+			log.Println("Connection closed", ew)
+			break
+		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("pgredis started on 127.0.0.1:6379")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error on accept: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module pgredis
+
+go 1.12
+
+require (
+	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/secmask/go-redisproto v0.0.0-20190520094750-7963d3e44e33
+	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	golang.org/x/tools v0.0.0-20190813214729-9dba7caff850 // indirect
+)

d7fdeb3351489a071a7d37fcf7b43a2f1e787c1a
James Healy <james@yob.id.au>
2019-08-14 21:50:58 +1000

    add a cli framework for easy command line scaffolding
    github.com/urfave/cli is nice and easy

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,63 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"strings"
 
 	"github.com/secmask/go-redisproto"
+	"github.com/urfave/cli"
 )
+
+func logf(msg string, args ...interface{}) {
+	_, err := fmt.Fprintf(os.Stderr, msg+"\n", args...)
+
+	if err != nil {
+		panic("failed to write to stderr: " + err.Error())
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "pgredis"
+	app.Usage = "Redis in front, postgresql out back"
+	app.Version = "dev"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "server",
+			Usage: "start the pgredis server",
+			Action: func(ctx *cli.Context) error {
+				return startServer()
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		logf("%+v", err)
+		os.Exit(1)
+	}
+}
+
+func startServer() error {
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("pgredis started on 127.0.0.1:6379")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error on accept: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
@@ -45,18 +96,3 @@ func handleConnection(conn net.Conn) {
 	}
 }
 
-func main() {
-	listener, err := net.Listen("tcp", ":6379")
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("pgredis started on 127.0.0.1:6379")
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println("Error on accept: ", err)
-			continue
-		}
-		go handleConnection(conn)
-	}
-}

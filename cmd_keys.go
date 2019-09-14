73912bbcb174c33ee51fa0df56eb5f5326e985dd
James Healy <james@yob.id.au>
2019-09-14 22:57:19 +1000

    comment of out mots commands
    Just leave the minimal set required for our transactions specs

--- cmd_keys.go
+++ cmd_keys.go
@@ -1,92 +1,92 @@
 package pgredis
 
-import (
-	"log"
-	"strconv"
+//import (
+//	"log"
+//	"strconv"
+//
+//	"github.com/secmask/go-redisproto"
+//)
 
-	"github.com/secmask/go-redisproto"
-)
-
-type delCommand struct{}
-
-func (cmd *delCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	result := int64(0)
-	for i := 1; i < command.ArgCount(); i++ {
-		// TODO calling Delete in a loop like this returns the correct result, but is super
-		//      inefficient. It'd be better to delete them in a single SQL call
-		success, err := redis.keys.Delete(command.Get(i))
-		if err != nil {
-			log.Println("ERROR: ", err.Error())
-		}
-		if success {
-			result += 1
-		}
-	}
-	return writer.WriteInt(result)
-}
-
-type existsCommand struct{}
-
-func (cmd *existsCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	result := int64(0)
-	for i := 1; i < command.ArgCount(); i++ {
-		success, err := redis.keys.Exist(command.Get(i))
-		if err != nil {
-			log.Println("ERROR: ", err.Error())
-		}
-		if success {
-			result += 1
-		}
-	}
-	return writer.WriteInt(result)
-}
-
-type expireCommand struct{}
-
-func (cmd *expireCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-	seconds, _ := strconv.Atoi(string(command.Get(2)))
-
-	success, err := redis.keys.SetExpire(key, seconds*1000)
-	if success {
-		return writer.WriteInt(1)
-	} else {
-		if err != nil {
-			log.Println("ERROR: ", err.Error())
-		}
-		return writer.WriteInt(0)
-	}
-}
-
-type ttlCommand struct{}
-
-func (cmd *ttlCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-	// this should probably use KeyRepository and not be string specific
-	success, resp, err := redis.strings.Get(key)
-	if success && resp.WillExpire() {
-		return writer.WriteInt(resp.TTLInSeconds())
-	} else if success {
-		return writer.WriteInt(-1) // the key exists, but it won't expire
-	} else if !success && err == nil {
-		return writer.WriteInt(-2) // the key didn't exist
-	} else {
-		panic(err) // TODO ergh
-	}
-}
-
-type typeCommand struct{}
-
-func (cmd *typeCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-	result, err := redis.keys.Type(key)
-	if err != nil {
-		log.Println("ERROR: ", err.Error())
-		return writer.WriteBulk(nil)
-	}
-	if result != "" {
-		return writer.WriteBulkString(result)
-	} else {
-		return writer.WriteBulkString("none")
-	}
-}
+//type delCommand struct{}
+//
+//func (cmd *delCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	result := int64(0)
+//	for i := 1; i < command.ArgCount(); i++ {
+//		// TODO calling Delete in a loop like this returns the correct result, but is super
+//		//      inefficient. It'd be better to delete them in a single SQL call
+//		success, err := redis.keys.Delete(command.Get(i))
+//		if err != nil {
+//			log.Println("ERROR: ", err.Error())
+//		}
+//		if success {
+//			result += 1
+//		}
+//	}
+//	return writer.WriteInt(result)
+//}
+//
+//type existsCommand struct{}
+//
+//func (cmd *existsCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	result := int64(0)
+//	for i := 1; i < command.ArgCount(); i++ {
+//		success, err := redis.keys.Exist(command.Get(i))
+//		if err != nil {
+//			log.Println("ERROR: ", err.Error())
+//		}
+//		if success {
+//			result += 1
+//		}
+//	}
+//	return writer.WriteInt(result)
+//}
+//
+//type expireCommand struct{}
+//
+//func (cmd *expireCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//	seconds, _ := strconv.Atoi(string(command.Get(2)))
+//
+//	success, err := redis.keys.SetExpire(key, seconds*1000)
+//	if success {
+//		return writer.WriteInt(1)
+//	} else {
+//		if err != nil {
+//			log.Println("ERROR: ", err.Error())
+//		}
+//		return writer.WriteInt(0)
+//	}
+//}
+//
+//type ttlCommand struct{}
+//
+//func (cmd *ttlCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//	// this should probably use KeyRepository and not be string specific
+//	success, resp, err := redis.strings.Get(key)
+//	if success && resp.WillExpire() {
+//		return writer.WriteInt(resp.TTLInSeconds())
+//	} else if success {
+//		return writer.WriteInt(-1) // the key exists, but it won't expire
+//	} else if !success && err == nil {
+//		return writer.WriteInt(-2) // the key didn't exist
+//	} else {
+//		panic(err) // TODO ergh
+//	}
+//}
+//
+//type typeCommand struct{}
+//
+//func (cmd *typeCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//	result, err := redis.keys.Type(key)
+//	if err != nil {
+//		log.Println("ERROR: ", err.Error())
+//		return writer.WriteBulk(nil)
+//	}
+//	if result != "" {
+//		return writer.WriteBulkString(result)
+//	} else {
+//		return writer.WriteBulkString("none")
+//	}
+//}
--- cmd_zsets.go
+++ cmd_zsets.go
@@ -1,101 +1,101 @@
 package pgredis
 
-import (
-	"log"
-	"strconv"
-
-	"github.com/secmask/go-redisproto"
-)
-
-type zaddCommand struct{}
-
-func (cmd *zaddCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	xxArgProvided := false
-	nxArgProvided := false
-	chArgProvided := false
-	incrArgProvided := false
-	lastArg := ""
-	values := make(map[string]float64)
-	key := command.Get(1)
-
-	for i := 2; i < command.ArgCount(); i++ {
-		if lastArg != "" { // the previous arg was a score, so this must be a member
-			score, _ := strconv.ParseFloat(lastArg, 64)
-			values[string(command.Get(i))] = score
-			lastArg = ""
-		} else if string(command.Get(i)) == "XX" {
-			xxArgProvided = true
-		} else if string(command.Get(i)) == "NX" {
-			nxArgProvided = true
-		} else if string(command.Get(i)) == "CH" {
-			chArgProvided = true
-		} else if string(command.Get(i)) == "INCR" {
-			incrArgProvided = true
-		} else { // must be a score
-			lastArg = string(command.Get(i))
-		}
-	}
-
-	log.Printf("xxArgProvided: %v", xxArgProvided)
-	log.Printf("nxArgProvided: %v", nxArgProvided)
-	log.Printf("incrArgProvided: %v", incrArgProvided)
-	log.Printf("values: %v", values)
-
-	updated, err := redis.sortedsets.Add(key, values, chArgProvided)
-	if err != nil {
-		log.Println("ERROR: ", err.Error())
-		return writer.WriteBulk(nil)
-	} else {
-		return writer.WriteInt(updated)
-	}
-}
-
-type zcardCommand struct{}
-
-func (cmd *zcardCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-
-	count, err := redis.sortedsets.Cardinality(key)
-	if err != nil {
-		log.Println("ERROR: ", err.Error())
-		return writer.WriteBulk(nil)
-	} else {
-		return writer.WriteInt(count)
-	}
-}
-
-type zrangeCommand struct{}
-
-func (cmd *zrangeCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-	start, _ := strconv.Atoi(string(command.Get(2)))
-	end, _ := strconv.Atoi(string(command.Get(3)))
-	includeScores := string(command.Get(4)) == "WITHSCORES"
-
-	items, err := redis.sortedsets.Range(key, start, end, includeScores)
-	if err == nil {
-		return writer.WriteBulkStrings(items)
-	} else {
-		log.Println("ERROR: ", err.Error())
-		return writer.WriteBulk(nil)
-	}
-}
-
-type zremCommand struct{}
-
-func (cmd *zremCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
-	key := command.Get(1)
-	values := make([][]byte, 0)
-	for i := 2; i < command.ArgCount(); i++ {
-		values = append(values, command.Get(i))
-	}
-
-	updated, err := redis.sortedsets.Remove(key, values)
-
-	if err != nil {
-		log.Println("ERROR: ", err.Error())
-		return writer.WriteError(err.Error())
-	} else {
-		return writer.WriteInt(updated)
-	}
-}
+//import (
+//	"log"
+//	"strconv"
+//
+//	"github.com/secmask/go-redisproto"
+//)
+//
+//type zaddCommand struct{}
+//
+//func (cmd *zaddCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	xxArgProvided := false
+//	nxArgProvided := false
+//	chArgProvided := false
+//	incrArgProvided := false
+//	lastArg := ""
+//	values := make(map[string]float64)
+//	key := command.Get(1)
+//
+//	for i := 2; i < command.ArgCount(); i++ {
+//		if lastArg != "" { // the previous arg was a score, so this must be a member
+//			score, _ := strconv.ParseFloat(lastArg, 64)
+//			values[string(command.Get(i))] = score
+//			lastArg = ""
+//		} else if string(command.Get(i)) == "XX" {
+//			xxArgProvided = true
+//		} else if string(command.Get(i)) == "NX" {
+//			nxArgProvided = true
+//		} else if string(command.Get(i)) == "CH" {
+//			chArgProvided = true
+//		} else if string(command.Get(i)) == "INCR" {
+//			incrArgProvided = true
+//		} else { // must be a score
+//			lastArg = string(command.Get(i))
+//		}
+//	}
+//
+//	log.Printf("xxArgProvided: %v", xxArgProvided)
+//	log.Printf("nxArgProvided: %v", nxArgProvided)
+//	log.Printf("incrArgProvided: %v", incrArgProvided)
+//	log.Printf("values: %v", values)
+//
+//	updated, err := redis.sortedsets.Add(key, values, chArgProvided)
+//	if err != nil {
+//		log.Println("ERROR: ", err.Error())
+//		return writer.WriteBulk(nil)
+//	} else {
+//		return writer.WriteInt(updated)
+//	}
+//}
+//
+//type zcardCommand struct{}
+//
+//func (cmd *zcardCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//
+//	count, err := redis.sortedsets.Cardinality(key)
+//	if err != nil {
+//		log.Println("ERROR: ", err.Error())
+//		return writer.WriteBulk(nil)
+//	} else {
+//		return writer.WriteInt(count)
+//	}
+//}
+//
+//type zrangeCommand struct{}
+//
+//func (cmd *zrangeCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//	start, _ := strconv.Atoi(string(command.Get(2)))
+//	end, _ := strconv.Atoi(string(command.Get(3)))
+//	includeScores := string(command.Get(4)) == "WITHSCORES"
+//
+//	items, err := redis.sortedsets.Range(key, start, end, includeScores)
+//	if err == nil {
+//		return writer.WriteBulkStrings(items)
+//	} else {
+//		log.Println("ERROR: ", err.Error())
+//		return writer.WriteBulk(nil)
+//	}
+//}
+//
+//type zremCommand struct{}
+//
+//func (cmd *zremCommand) Execute(command *redisproto.Command, redis *PgRedis, writer *redisproto.Writer) error {
+//	key := command.Get(1)
+//	values := make([][]byte, 0)
+//	for i := 2; i < command.ArgCount(); i++ {
+//		values = append(values, command.Get(i))
+//	}
+//
+//	updated, err := redis.sortedsets.Remove(key, values)
+//
+//	if err != nil {
+//		log.Println("ERROR: ", err.Error())
+//		return writer.WriteError(err.Error())
+//	} else {
+//		return writer.WriteInt(updated)
+//	}
+//}
